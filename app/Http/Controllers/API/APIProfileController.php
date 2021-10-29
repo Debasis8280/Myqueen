523fea1fdc2bf449107d7f837b838eeaebe34f25
Debasis <debasissahoo8280@gmail.com>
2021-10-29 00:50:18 -0700

    add thanks api

--- app/Http/Controllers/API/APIProfileController.php
+++ app/Http/Controllers/API/APIProfileController.php
@@ -13,9 +13,10 @@ class APIProfileController extends Controller
     public function profile_data()
     {
         $data = User::find(request()->user()->id);
-        return response([
-            $data
-        ], 201);
+        return response(
+            $data,
+            201
+        );
     }
 
     public function get_pv_point()
